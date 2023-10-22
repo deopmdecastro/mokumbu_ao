788d72131e29cead6b9b02aae6f7b9f3df18b787
deopmdecastro <deopmdecastro@gmail.com>
2023-10-22 17:26:19 +0100

    Perguntas Frequentes

---
+++ assets/js/mokumbu.js
@@ -0,0 +1,9 @@
+function toggleAnswer(questionNumber) {
+    var answer = document.getElementById("faq-answer-" + questionNumber);
+  
+    if (answer.style.display === "none" || answer.style.display === "") {
+      answer.style.display = "block";
+    } else {
+      answer.style.display = "none";
+    }
+}  
