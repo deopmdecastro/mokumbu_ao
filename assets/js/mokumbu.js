135491ddd517fca3169eb4ca0d08d571b03b4058
deopmdecastro <deopmdecastro@gmail.com>
2023-10-24 09:52:54 +0100

    https://mokumbu.netlify.app/

--- assets/js/mokumbu.js
+++ assets/js/mokumbu.js
@@ -1,9 +1,14 @@
 function toggleAnswer(questionNumber) {
     var answer = document.getElementById("faq-answer-" + questionNumber);
-  
+
+
     if (answer.style.display === "none" || answer.style.display === "") {
-      answer.style.display = "block";
+        answer.style.display = "block";
+      
+
     } else {
-      answer.style.display = "none";
+        answer.style.display = "none";
+      
     }
-}  
+}
+
